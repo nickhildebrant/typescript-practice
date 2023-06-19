ec27926e9e9fc01c1e347607708fcf211c81f462
nickhildebrant <nickhildebrant09@gmail.com>
2023-06-19 11:14:44 -0700

    learning some syntax now

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,7 @@
 "use strict";
 let message = "Hello, World!";
 console.log(message);
+let num1 = 6;
+num1 += 2;
+console.log(num1);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 let message : string = "Hello, World!";
 console.log(message);
+
+let num1 : number = 6;
+num1 += 2;
+console.log(num1);
