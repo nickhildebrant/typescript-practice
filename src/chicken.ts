87fdda230bf06fb1c82833da6820b0512a481835
nickhildebrant <nickhildebrant09@gmail.com>
2023-06-19 10:34:30 -0700

    Added Typescript watch process
    This is so I don't have to keep compiling after each change and can easily use the debugger

---
+++ src/chicken.ts
@@ -0,0 +1,7 @@
+export class Chicken
+{
+    cluck()
+    {
+        console.log("cluck");
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { Chicken } from "./chicken";
+
+console.log("Hello, World!");
+
+const chicken = new Chicken();
+chicken.cluck();
