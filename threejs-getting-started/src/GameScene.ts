a81e4199bc23cfa737ab4483331a89dc018c6ecd
nickhildebrant <nickhildebrant09@gmail.com>
2023-06-20 13:05:02 -0700

    Created A simple scene

--- threejs-getting-started/src/GameScene.ts
+++ threejs-getting-started/src/GameScene.ts
@@ -1,20 +1,46 @@
 import * as THREE from 'three'
 
+import { Object3D } from 'three';
+import { MaterialLoader } from 'three';
+
 export default class GameScene extends THREE.Scene
 {
+    private readonly objectLoader = new THREE.ObjectLoader()
+    private readonly materialLoader = new THREE.MaterialLoader()
+
     initializeScene()
     {
         const cubeGemometry = new THREE.BoxGeometry()
-        const phongMaterial = new THREE.MeshPhongMaterial({color: 0xFFAD00})
+        const sphereGeometry = new THREE.SphereGeometry()
 
-        const cube = new THREE.Mesh(cubeGemometry, phongMaterial);
-        cube.position.z = -5
-        cube.position.y = -1
-        this.add(cube);
+        const redMaterial = new THREE.MeshPhongMaterial({color: 0xFF0000})
+        const greenMaterial = new THREE.MeshPhongMaterial({color: 0x00FF00})
+        const whiteMaterial = new THREE.MeshPhongMaterial({color: 0xFFFFFF})
 
-        const light = new THREE.DirectionalLight(0xFFFFFF, 1)
-        light.position.set(0, 4, 2)
-        this.add(light)
+        const leftPaddle = new THREE.Mesh(cubeGemometry, redMaterial);
+        leftPaddle.position.x = 2
+        leftPaddle.position.y = 0
+        leftPaddle.position.z = -10
+
+        const rightPaddle = new THREE.Mesh(cubeGemometry, greenMaterial);
+        rightPaddle.position.x = -2
+        rightPaddle.position.y = 0
+        rightPaddle.position.z = -10
+
+        const ball = new THREE.Mesh(sphereGeometry, whiteMaterial)
+        ball.position.x = 0
+        ball.position.y = 0
+        ball.position.z = -10
+
+        this.add(leftPaddle, rightPaddle, ball)
+
+        const lightLeft = new THREE.DirectionalLight(0xFFFFFF, 1)
+        lightLeft.position.set(1, 2, 2)
+
+        const lightRight = new THREE.DirectionalLight(0xFFFFFF, 1)
+        lightRight.position.set(-1, 2, 2)
+
+        this.add(lightLeft, lightRight)
     }
 
     update()
